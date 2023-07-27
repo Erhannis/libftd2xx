38ea4bee3e6300683bafd45265f143d5e0040b56
erhannis <eyeillus@gmail.com>
2023-07-27 11:15:38 -0400

    read data

--- examples/read.rs
+++ examples/read.rs
@@ -5,38 +5,44 @@
 //! On the FT232H this will toggle ADBUS0 from high to low.
 //!
 //! [MPSSE Basics]: https://www.ftdichip.com/Support/Documents/AppNotes/AN_135_MPSSE_Basics.pdf
-#![deny(unsafe_code, warnings)]
-use libftd2xx::{BitMode, Ft232h, Ftdi, FtdiCommon, FtdiMpsse};
+#![deny(unsafe_code)]
+use libftd2xx::{BitMode, Ft232h, Ftdi, FtdiCommon};
 use std::error::Error;
+//use std::thread;
 use std::time::Duration;
+
+const RX_BUF_SIZE: usize = 0x10000;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut ft: Ft232h = Ftdi::new()?.try_into()?;
+    let mut rx_buf: Vec<u8> = vec![0; RX_BUF_SIZE];
 
     ft.reset()?;
     ft.purge_all()?;
     debug_assert_eq!(ft.queue_status()?, 0);
-    ft.set_usb_parameters(65536)?;
+    //ft.set_usb_parameters(65536)?;
     ft.set_chars(0, false, 0, false)?;
-    ft.set_timeouts(Duration::from_millis(1000), Duration::from_millis(1000))?;
-    ft.set_latency_timer(Duration::from_millis(2))?;
+    ft.set_timeouts(Duration::from_millis(10000), Duration::from_millis(10000))?;
+    ft.set_latency_timer(Duration::from_millis(16))?;
     ft.set_flow_control_rts_cts()?;
-    ft.set_bit_mode(0x0, BitMode::Reset)?;
-    ft.set_bit_mode(0x0, BitMode::Mpsse)?;
+    ft.set_bit_mode(0x00, BitMode::Reset)?;
+    ft.set_bit_mode(0x00, BitMode::SyncFifo)?; //CHECK mask? 0xFF instead, like other code?
+    //ft.set_flow_control_rts_cts()?;
 
     // From the application note "Wait for all the USB stuff to complete and work"
     // This does not seem to be necessary though
     // thread::sleep(Duration::from_millis(100));
 
-    ft.enable_loopback()?;
-    ft.synchronize_mpsse()?;
-    ft.disable_loopback()?;
-
-    // Toggle AD0
-    for _ in 0..80000 {
-        //print!(".");
-        ft.set_gpio_lower(0xFF, 0xFF)?;
-        ft.set_gpio_lower(0xFE, 0xFF)?;
+    for _ in 0..10 {
+        print!(".");
+        ft.read_all(&mut rx_buf)?;
+        print!("rx: ");
+        for i in 0..rx_buf.len() {
+            print!("{},", rx_buf[i]);
+        }
+        println!();
+        println!();
+        //ft.write_all(&rx_buf)?;
     }
 
     println!();
@@ -45,3 +51,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+
+
