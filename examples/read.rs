dc0d6d5a5f26a9e7f996a6746f091dc9246e709e
erhannis <eyeillus@gmail.com>
2023-08-01 02:03:30 -0400

    Reducing speed from 280 to 240 increases throughput to 7MByte/s, ...and I forgot to commit, and what I was going to say.  It looks like the output still skips a bunch of entries, though?  Kindof erratically.  65,66,68,69,73,74,76

--- examples/read.rs
+++ examples/read.rs
@@ -9,9 +9,13 @@
 use libftd2xx::{BitMode, Ft232h, Ftdi, FtdiCommon};
 use std::error::Error;
 //use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::cmp::min;
 
-const RX_BUF_SIZE: usize = 0x10000;
+const RX_BUF_SIZE: usize = 0x10000*3;//*0x200
+const MAX_PRINT_SIZE: usize = 0x10;
+// const MAX_PRINT_SIZE: usize = 0x1000000;
+const ITER: i32 = 0x10;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut ft: Ft232h = Ftdi::new()?.try_into()?;
@@ -20,25 +24,39 @@ fn main() -> Result<(), Box<dyn Error>> {
     ft.reset()?;
     ft.purge_all()?;
     debug_assert_eq!(ft.queue_status()?, 0);
-    //ft.set_usb_parameters(65536)?;
+    ft.set_usb_parameters(65536)?;
     ft.set_chars(0, false, 0, false)?;
     ft.set_timeouts(Duration::from_millis(10000), Duration::from_millis(10000))?;
-    ft.set_latency_timer(Duration::from_millis(16))?;
-    ft.set_flow_control_rts_cts()?;
+    //ft.set_latency_timer(Duration::from_millis(16))?;
+    //ft.set_flow_control_rts_cts()?;
     ft.set_bit_mode(0x00, BitMode::Reset)?;
     ft.set_bit_mode(0x00, BitMode::SyncFifo)?; //CHECK mask? 0xFF instead, like other code?
-    //ft.set_flow_control_rts_cts()?;
 
     // From the application note "Wait for all the USB stuff to complete and work"
     // This does not seem to be necessary though
     // thread::sleep(Duration::from_millis(100));
 
-    for _ in 0..10 {
-        print!(".");
+    for _ in 0..ITER {
+        print!(". ");
+        let now = Instant::now();
         ft.read_all(&mut rx_buf)?;
+        let t = now.elapsed().as_micros();
+        let z: u128 = (RX_BUF_SIZE * 1000000).try_into().unwrap();
+        println!("{RX_BUF_SIZE} @ {} = {} B/s", t, z/t);
         print!("rx: ");
-        for i in 0..rx_buf.len() {
-            print!("{},", rx_buf[i]);
+        let n = min(rx_buf.len(), MAX_PRINT_SIZE);
+        if n < rx_buf.len() {
+            for i in 0..n {
+                print!("{:#03},", rx_buf[i]);
+            }
+            print!("...");
+            for i in (rx_buf.len()-n)..rx_buf.len() {
+                print!("{:#03},", rx_buf[i]);
+            }
+        } else {
+            for i in 0..n {
+                print!("{:#03},", rx_buf[i]);
+            }
         }
         println!();
         println!();
